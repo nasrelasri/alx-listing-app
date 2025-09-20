e5ba74508edcb5994bc4ec2ca0e7e1859e5232e0
Nasr El Asri <nasr.asri10@gmail.com>
2025-09-20 17:34:36 +0100

    Build Responsive Listing Page with Tailwind CSS

--- interfaces/index.ts
+++ interfaces/index.ts
@@ -11,3 +11,27 @@ export interface ButtonProps {
   onClick: () => void;
   variant?: "primary" | "secondary";
 }
+
+// Layout Component Props
+export interface LayoutProps {
+  children: React.ReactNode;
+}
+
+export interface PropertyProps {
+  name: string;
+  address: {
+    state: string;
+    city: string;
+    country: string;
+  };
+  rating: number;
+  category: string[];
+  price: number;
+  offers: {
+    bed: string;
+    shower: string;
+    occupants: string;
+  };
+  image: string;
+  discount: string;
+}
